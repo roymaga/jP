a0b65d405c5444face757105fd38b920437997ff
ezeswci <ezequiel@swci.com.ar>
2016-02-01 18:07:07 -0300

    mmm cookies

--- dist/js/handleCookies.js
+++ dist/js/handleCookies.js
@@ -1,22 +1,25 @@
 // JavaScript Document
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
+    //var d = new Date();
+    //d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    //var expires = "expires="+d.toUTCString();
+   // document.cookie = cname + "=" + cvalue + "; " + expires;
+	 window.localStorage.setItem(cname, cvalue);
 }
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
+    //var name = cname + "=";
+   // var ca = document.cookie.split(';');
+   var ca = window.localStorage.getItem(cname);
+    /*for(var i=0; i<ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0)==' ') c = c.substring(1);
         if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
+    }*/
+    return ca;
 }
 function delete_cookie( name ) {
-  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  //document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  window.localStorage.removeItem(name);
 }
 function checkCookie(){
 	if(navigator.cookieEnabled==true){
@@ -24,10 +27,12 @@ function checkCookie(){
 		if(document.cookie.indexOf("testcookie")!=-1){
 			return true;
 		}else{
-			return false;
+			//return false;
+			return true;
 		}
 		
 	}else{
-		return false;
+		//return false;
+		return true;
 	}
 }
