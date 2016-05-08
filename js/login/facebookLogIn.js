82bd02670f8d32ca0b40fa90bd3088dc37ff8c06
ezeswci <ezequiel@swci.com.ar>
2016-05-08 19:55:15 -0300

    log in facebook

--- js/login/facebookLogIn.js
+++ js/login/facebookLogIn.js
@@ -1,4 +1,19 @@
 // JavaScript Document
 function openFabookConect(url){
-		 window.open(url);
+		 var myWindow = window.open(url);
+		 setTimeout(function(){ checkOpenedFacebookWindow(myWindow); }, 3000);
+}
+function checkOpenedFacebookWindow(myWindow){
+	// myWindow.close(); Cierra la ventana
+	dir=myWindow.URL;
+	if(dir!=null){
+		if(dir.indexOf('app.jugaplay.com/api/v1') == -1 && dir.indexOf('jugaplay.com') != -1){
+			myWindow.close();
+			alert("check log in");
+		}else{
+			setTimeout(function(){ checkOpenedFacebookWindow(myWindow); }, 3000);
+		}
+	}else{
+		alert("se cerro la ventana");
+	}
 }
