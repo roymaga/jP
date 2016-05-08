c0a77dd4c398d10e465dd48a8de0c0035ec7a6e8
ezeswci <ezequiel@swci.com.ar>
2016-05-08 19:40:38 -0300

    facebook test 4

--- js/login/facebookLogIn.js
+++ js/login/facebookLogIn.js
@@ -1,16 +1,4 @@
 // JavaScript Document
 function openFabookConect(url){
-	BootstrapDialog.show({
-			 cssClass: 'log-in-pop-up',
-			 title: "Fabook Log In",
-            message: '<iframe src="http://app.jugaplay.com/api/v1/users/auth/facebook?invited_by=1"></iframe>',
-			buttons: [{
-                label: 'Log In',
-				id:'boton-panel-login',
-                action: function(dialog) {
-                    logInUsuarioEnElSitio();
-                }
-            }]
-		 
-		 });
+		 window.open(url);
 }
