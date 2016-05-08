860f8b92f3574cece555edc183df34c7383b6f67
ezeswci <ezequiel@swci.com.ar>
2016-05-08 19:27:20 -0300

    Test facebook 2

--- js/login/facebookLogIn.js
+++ js/login/facebookLogIn.js
@@ -3,7 +3,7 @@ function openFabookConect(url){
 	BootstrapDialog.show({
 			 cssClass: 'log-in-pop-up',
 			 title: "Fabook Log In",
-            message: '<iframe src="'+url+'"></iframe>',
+            message: '<iframe src="http://app.jugaplay.com/api/v1/users/auth/facebook?invited_by=1"></iframe>',
 			buttons: [{
                 label: 'Log In',
 				id:'boton-panel-login',
